60579a12d5c54054ceeebb233487d6882fb59137
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2020-06-05 18:58:04 +0000

    feat: add react router to main page
    react router add for index and main page

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+	return(
+    	<div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+       		</div>
+          </div>
+    )
+}
+
+export default Title
